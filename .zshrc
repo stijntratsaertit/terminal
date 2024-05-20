37bb1b3856b6e1dfe727e8da63f857276455e008
Stijn Tratsaert <stijn.tratsaert.it@gmail.com>
2024-05-20 18:22:23 +0200

    add .zshrc and setup.sh

---
+++ .zshrc
@@ -0,0 +1,31 @@
+# Load oh-my-zsh
+source $ZSH/oh-my-zsh.sh
+
+# Path to your oh-my-zsh installation.
+export ZSH="~/.oh-my-zsh"
+
+# Set name of the theme to load.
+ZSH_THEME="spaceship"
+
+# Set auto-update behavior
+zstyle ':omz:update' mode auto
+
+# Set auto-update frequence (in days)
+zstyle ':omz:update' frequency 14
+
+# Set list of plugins to load
+plugins=(
+terraform
+git
+docker
+kubectl
+pyenv
+python
+docker-compose
+zsh-syntax-highlighting
+zsh-autosuggestions
+fzf
+)
+
+# Set spaceship theme options
+SPACESHIP_DOCKER_SHOW=false
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install dependencies
+apt update && apt install -y \
+    curl
+    zsh
+    git
+    fzf
+
+# Install oh-my-zsh
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install powerline-fonts
+cd powerline-fonts && ./install.sh
+
+# Install spaceship theme
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Create symlinks
+ln -s -f $(pwd)/.zshrc ~/.zshrc
+
+# Install plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
